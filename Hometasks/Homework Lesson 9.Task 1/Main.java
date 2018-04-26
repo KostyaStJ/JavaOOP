92367d80f1dfc409f57bf04a8fc5fcb874b61576
KostyaStJ <37849825+KostyaStJ@users.noreply.github.com>
2018-04-26 12:46:36 +0300

    Add files via upload

---
+++ Hometasks/Homework Lesson 9.Task 1/Main.java
@@ -0,0 +1,29 @@
+package hwnine;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Main {
+
+	public static void main(String[] args) {
+		List();
+
+	}
+	public static void List () {
+		List<Integer>a = new ArrayList<>();
+		for (int i = 0; i < 10; i++) {
+			a.add(i);
+			
+		}
+		
+		a.remove(0);
+		a.remove(1);
+		a.remove(7);
+		
+		
+		System.out.println(Arrays.toString(a.toArray()));
+		
+	}
+
+}
